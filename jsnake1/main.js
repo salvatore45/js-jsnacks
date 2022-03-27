361a4d322000a3f7eb7ece351189261bcd753226
salvatore <tore0145@gmail.com>
2022-03-27 23:33:33 +0200

    primoesercizio

---
+++ jsnake1/main.js
@@ -0,0 +1,8 @@
+let numerouno= prompt("inserisci il primo numero");
+let numerodue= prompt("inserisci il secondo numero");
+
+if(numerouno>numerodue){
+    console.log(numerouno);
+} else{
+    console.log(numerodue);
+}
